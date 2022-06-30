f76109ab9ca8a2583a7d413ced951d11001dd22e
Ezinwa charles <charleskelvin12@gmail.com>
2022-06-30 04:24:39 -0700

    c hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Return:always 0 (success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,\n");
-return (0);
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }
