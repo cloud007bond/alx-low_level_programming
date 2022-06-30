a7e0751966de0067fb3fde6eb4436434549b75a9
Ezinwa charles <charleskelvin12@gmail.com>
2022-06-30 09:27:06 -0700

    c hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  * main - prints exactly " and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  *   followed by a new line, to the standard error.
- *   Return: always 0 (success)
- */int main (void)
+ *   Return: Always 0 (success)
+ */
+int main (void)
 {
 write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
 return (1);
