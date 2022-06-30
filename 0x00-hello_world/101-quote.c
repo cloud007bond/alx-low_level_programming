22aef34e8f93a005fd7c2c2864f03d87ac2217f1
Ezinwa charles <charleskelvin12@gmail.com>
2022-06-30 10:10:39 -0700

    c hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints exactly and that piece of art is useful
- * -Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
+ * -Dora Korpar, 2015-10-19
+ * followed by a new line to the standard error.
  * Return: Always 0 (success)
  */
 int main (void)
