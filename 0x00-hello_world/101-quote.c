580587806589c9abac118ce268a3de451c037ac9
Ezinwa charles <charleskelvin12@gmail.com>
2022-06-30 10:28:44 -0700

    c hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful
- * -Dora Korpar, 2015-10-19
- * followed by a new line to the standard error.
- * Return: Always 0 (success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
-int main (void)
+int main(void)
 {
-write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
 return (1);
 }
