d6009e895c110248e22aca1d5215b098aee7008e
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-01 10:00:46 -0700

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print some letters of alphabet
+ *
+ * Return : always (success)
+ */
+int main(void)
+{
+       char c;
+
+       for (c = 'a'; c <= 'z'; c++)
+
+       {
+	    if(c != 'e' && c != 'q')
+
+	    {
+		    putchar(c);
+	    }
+       }
+     
+       putchar('\n');
+
+       return (0);
+ 
+}       
+
+	        	    
