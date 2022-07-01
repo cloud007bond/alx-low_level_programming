d40be4ddc9e3b86354b5e0d60d066eb03a20c05d
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-01 14:30:07 -0700

    possible different combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,56 +1,41 @@
 #include <stdio.h>
 
-
-
 /**
  * main - Prints 3 combination of numbers
  *
  * Return: Always (Success)
  */
 
-int main(void)
-
+int main(void0
 {
+  int c, i , k;
 
-		int c, i, k;
+  for (c = '0'; c <= '9'; c++)
+  {
+    for(i = '0'; i <= '9'; i++)
+    {
+      for(k = '0'; k <= '9; k++)
+      { 
+         for (c < i && i < k)
+	 {
+	     putchar(c);
+	     putchar(i);
+	     putchar(k);
 
-		for (c = '0'; c <= '9'; c++)
+	     if(c != '7')
+	     {
+	       putchar(',');
+	       putchar(' ');
+	     }
+	 }
 
-		{
-                        for (i = '0'; i <= '9'; i++)
-
-			{
-
-			       for (k = '0'; k <= '9'; k++)
-
-				{
-				      if (c < i && i < k)
-
-				       {
-
-															    putchar(c);
-
-					    putchar(i);
-
-					    putchar(k);
+	 
+     }
 
 
-                                            if (c != '7')
-					    {
+  } 
+	
 
-					        putchar(',');
-
-						putchar(' ');
-
-															    }
-
-														}
-
-											               }
-
-														
-
-	      }
 
 
 
