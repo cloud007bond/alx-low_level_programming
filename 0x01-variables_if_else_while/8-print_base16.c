5b54ce04fad066e5fe8b3eaed622ae54dfbbc39c
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-03 03:28:04 -0700

    all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,29 +7,19 @@
  */
 int main(void)
 {
+	char c;
 
-	    char c;
-
-            for (c = '0'; c <= '9'; c++)
-            
-	    {
+	for (c = '0'; c <= '9'; c++)
+	{
 
 		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
 
-	    }
-
-
-           for (c = 'a'; c <= 'f'; c++)
-
-	    {
-
-	        putchar(c);
-
-	    }  
-
-              putchar('\n');
-
-
-              return (0);
-
+	putchar('\n');
+	return (0);
 }
+
