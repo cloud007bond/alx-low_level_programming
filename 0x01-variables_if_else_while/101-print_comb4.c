fbffdf9ca064a8a1f3132946de2968de493a6e14
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-03 07:34:14 -0700

    all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,10 +5,11 @@
  * * main - Entry point
  * *
  * * Return: Always 0
- * */
+ */
 int main(void)
 {
 	int digit1, digit2, digit3;
+
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
