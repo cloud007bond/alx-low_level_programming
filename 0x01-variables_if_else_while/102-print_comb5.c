91674c615abb00c0d6f69ebe74a99a98693848ca
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-03 07:29:34 -0700

    possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int num1, num2;
+
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -25,3 +26,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
