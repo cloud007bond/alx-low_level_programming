b8f3501f9df783567d70544d3ffdf76bc48d7fe6
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-05 03:39:03 -0700

    c function and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print lower case of an alphabet
+ * Description: wriet a code that print lower acseof alphabet
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
+
