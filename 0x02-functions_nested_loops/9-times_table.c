63862ff91cfa50e668195419373bd10d33e48a12
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-05 20:29:09 -0700

    hot fix

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * time_table - print the 9 times table
+ * Description print the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, column, product, tens, ones;
+
+	for (roe = 0; row <= 9; row++)
+
+	{
+		for (column = 0; column <= 9; column ++0)
+		{	product = row * colimn;
+			tens = produt / 10;
+			ones = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+
+				_putchar(' ');
+
+				_putchar(' ');
+
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
