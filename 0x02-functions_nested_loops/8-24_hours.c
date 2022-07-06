da50e0171e97bcf926b37d53d731e2326721fc93
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-05 22:00:30 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,38 +1,44 @@
 nclude "main.h"
 
 /**
- * jack_bauer -> print 24 hours
- *
- *
+ * jack_bauer - prints every minute of the day
+ *Description: prints every minute of the day
+ *Return: void
  */
 void jack_bauer(void)
 {
-	int i; j;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (i = 0; i < 24; i++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		for (j = 0; j < 60; j++)
+		if (hours_tens == '2')
 		{
-			for (i < 10)
+			hours_max = '4';
+		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_ones = '0';
+			while (minutes_ones < 58)
 			{
-				_putchar('0');
-				_putchar(1 + '0');
-			}
-			else if (i >= 10)
-				_putchar((i / 10) + '10
+				_putchar(hours_tens);
+				_putchar(hours_ones);
 				_putchar(':');
-				_putchar('0');
-				_putchar(j + '0');
+				_putchar(minutes_tens);
+				_putchar(minutes_ones);
+				_putchar('\n');
+				minutes_ones++;
 			}
-			else if (j > 10)
-			{	_putchar(':');
-				_putchar((j / 10) + '10');
-				_putchar((j % 10) + '10');
-			}
-		}	_putchar('\n');
-		
+			minutes_ones = '0';
+			minutes_tens++;
+		}
+		minutes_tens = '0';
+		hours_ones++;
 	}
+	hours_ones = '0';
+	hours_tens++;
 }
-
 
 
