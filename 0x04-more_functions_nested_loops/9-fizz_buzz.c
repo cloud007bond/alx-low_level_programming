eba7eca9576197fe47063523339522d677413d7a
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-07 06:19:41 -0700

    hot fix

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+/**
+ * main - print fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i == 100)
+		{
+			continue;
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
