6730504a1322a6df4a4e33b6a60db28a7e0aeae3
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-06 21:17:43 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 
 /**
  * jack_bauer - prints every minute of the day
- *Description: prints every minute of the day
- *Return: void
+ * Description: prints every minute of the day
+ * Return: void
  */
 void jack_bauer(void)
 {
