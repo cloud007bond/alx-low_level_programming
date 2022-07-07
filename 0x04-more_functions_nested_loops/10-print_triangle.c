4fd5138d2be7795ae6ada454f277aded00070f27
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-07 14:47:23 -0700

    hot fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,29 +5,30 @@
  * @size: numbers of triangle to print
  * Return: 0
  */
-
 void print_triangle(int size)
 {
-	int row, columns, hash;
+	int line = 0;
+	int spaces;
+	int hashes;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= size; row++)
+		while (line < size)
 		{
-			for (columns = 1; columns <= size - row; columns++)
+			for (spaces = size - 1; spaces > line; spaces--)
 			{
 				_putchar(' ');
 			}
-			for (hash = 1; hash <= row; hash++)
+			for (hashes = 0; hashes < line + 1; hashes++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
+			line++;
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
