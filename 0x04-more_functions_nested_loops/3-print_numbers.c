b5f26d3a7d99a0d3717ec9be90000799a34b9492
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-07 03:17:13 -0700

    hot fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print num fro zero to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 9; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
