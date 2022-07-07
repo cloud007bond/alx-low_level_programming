ded1c5431db9f82b1161a3c86a547d921af953bb
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-07 03:22:48 -0700

    hot fix

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most num exclude 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 9; n++)
+	{
+		if (n == 2 || n == 4)
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
