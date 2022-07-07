d050b9b8379fbfa362a7cd8acbe7e59c52e6eb95
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-07 13:21:45 -0700

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,9 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
+
 {
 int i;
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-
+#include <stdio.h>
 
 void positive_or_negative(int i);
 
