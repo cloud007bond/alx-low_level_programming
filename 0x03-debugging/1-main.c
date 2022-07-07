9f8d994b5966c3dc276f7fd4f9f31539786457fb
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-07 13:31:03 -0700

    hot fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,9 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
+
 {
 	int i;
 
@@ -18,6 +20,5 @@ int main(void)
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
