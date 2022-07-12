f89c6a626d6599ddda88c7af952cfc65298c892a
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-12 00:44:44 -0700

    resetting pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN H
+#define MAIN_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,4 +12,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#defif
 
