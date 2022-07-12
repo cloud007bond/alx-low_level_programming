b4b79a7f4f2b18959bbdc9edee971dac8333ffd3
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-12 01:33:35 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main,h"
+
+/**
+ * swap_int -> given two integersswap the value they are holding
+ * @a: parameter 1
+ * @b: paraneter 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	*a = *b
+	* b = tmp
+}
