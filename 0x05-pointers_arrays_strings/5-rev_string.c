e91b9aca8446d99ae5d91344acc04bdc789d185a
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-12 10:21:44 -0700

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,16 @@
 
 void rev_string(char *s)
 {
-	int k;
-	char *school = s;
+	int i = 0, len = 0;
+	char tmp;
 
-	for (k = strlen(s); k != '\0'; k--)
+	while (s[i++])
+		len++;
+	for (i = len - 1; i >= len / 2; i--)
 	{
-		*school = s[k];
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
-	printf("\n");
+
 }
