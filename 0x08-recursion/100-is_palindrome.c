0a737da217dc2df4bee0a8e6eb5da2086bad72aa
Ezinwa charles <charleskelvin12@gmail.com>
2022-07-19 09:20:36 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,18 +5,21 @@ int check_palindrome(char *s);
 /**
  * is_palindrome - Returns if a string is palindrome
  * @s: the string value to be checked
+ *
  * Return: integer value
  */
 int is_palindrome(char *s)
 {
 	if (*s == '0')
 		return (1);
+
 	return (check_palindrome(s));
 }
 
 /**
  * check_palindrome - Check if a string is palindrome
  * @s: the string value to be checked
+ *
  * Return: integer value
  */
 int check_palindrome(char *s)
@@ -32,11 +35,14 @@ int check_palindrome(char *s)
 	{
 		return (0);
 	}
+
 	return (1);
 }
+
 /**
  * _strlen_recursion - Get the length of a string
  * @s: the string to get the length
+ *
  * Return: the string length
  */
 int _strlen_recursion(char *s)
@@ -45,6 +51,7 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
+
 	s++;
 	return (_strlen_recursion(s) + 1);
 }
