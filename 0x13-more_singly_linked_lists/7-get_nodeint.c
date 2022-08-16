84b1f2185996bea1e79a2d736c8fd0ccbd3e323a
cloud007bond <charleskelvin12@gmail.com>
2022-08-16 00:20:37 -0700

    more singly list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,59 +1,32 @@
 #include "lists.h"
 
-/**
- * listint_len - Returns the number of nodes in a list
-
- * @h: The head of the list
-
- * Return: The number of nodes or 0
-
- */
-
-size_t listint_len(const listint_t *h)
-{
-	size_t numElem = 0;
-
-	while (h != NULL)
-	{
-		numElem++;
-		h = h->next;
-	}
-	return (numElem);
-}
-
-/**
- * get_nodeint_at_index - Returns the number at a particular node index
-
- * @head: Head of the list
-
- * @index: Index No of node
-
- * Return: Pointer to the node
-
- */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-
 {
 
-	size_t size = listint_len(head);
+	listint_t *temp;
 
-	listint_t *node = head;
+	unsigned int a = 0, count = 0;
 
-	size_t current = 0;
+	temp = head;
 
-	if (index > size || head == NULL)
-
-		return (NULL);
-
-	while (current < index)
-
+	if (head == NULL)
 	{
-
-		node = node->next;
-
-		current++;
-
+		return (NULL);
 	}
-	return (node);
+	while (temp)
+	{
+		temp = temp->next;
+		count++;
+
+		if (index > count)
+		{
+
+			return (NULL);
+		}
+	}
+	while (a < index)
+	{
+		temp = temp->next;
+	}
+	return (temp);
 }
