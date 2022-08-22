bb060762beb6b6ba673ac3232a63e984230b6305
cloud007bond <charleskelvin12@gmail.com>
2022-08-22 10:32:56 -0700

    file io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FILE_IO_
+#define _FILE_IO_
 
 
 
@@ -11,11 +11,11 @@
 #include <stdlib.h>
 
 
-
+int _putchar(char c)
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void copy_file(const char *src, const char *dest);
 
 
 #endif
