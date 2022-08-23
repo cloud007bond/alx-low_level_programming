e4c9d5c9a34174eaa02ca2a80029d2b05b3b8333
cloud007bond <charleskelvin12@gmail.com>
2022-08-22 18:25:52 -0700

    file io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,71 +1,39 @@
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-
 
 /**
  * append_text_to_file - appends text at the end of a file
-
- * @filename: name of file to append text to
-
- * @text_content: NULL terminated string to add at end of file
-
- * Return: 1 on success, -1 on failure
-
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
-
 {
+	int fd;
+	int nletters;
+	int rwr;
 
-	int fdo, fdw, len = 0;
-
-
-
-	if (filename == NULL)
-
+	if (!filename)
 		return (-1);
 
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-
-	fdo = open(filename, O_RDWR | O_APPEND);
-
-	if (fdo < 0)
-
+	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-
+	if (text_content)
 	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-		close(fdo);
+		rwr = write(fd, text_content, nletters);
 
-		return (1);
-
+		if (rwr == -1)
+			return (-1);
 	}
 
-
-
-	while (*(text_content + len))
-
-		len++;
-
-
-
-	fdw = write(fdo, text_content, len);
-
-	close(fdo);
-
-	if (fdw < 0)
-
-		return (-1);
-
-
+	close(fd);
 
 	return (1);
-
 }
