ce4b92effe01db087f258f89c9992a17436413a3
Ezinwa charles <charleskelvin12@gmail.com>
2022-09-29 08:26:20 -0700

    double link

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,15 +2,10 @@
 
 /**
  * dlistint_len - Counts the number of elements in a linked dlistint_t list.
-
  * @h: The head of the dlistint_t list.
-
  * Return: The number of elements in the dlistint_t list.
-
  */
-
 size_t dlistint_len(const dlistint_t *h)
-
 {
 
 	size_t nodes = 0;
